b7357053f15cb624c5aa338c31a56879f48b82b7
julian lago <julianlago93@gmail.com>
2020-06-22 14:33:54 -0300

    Agrega boton para cambiar moneda Base

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
 /// <reference types="jquery" />
 /// <reference types="sizzle"/>
 
-const URL = "https://api.exchangeratesapi.io/latest";
+const urlEuro = "https://api.exchangeratesapi.io/latest";
 let moneda ="";
-fetch(URL)
+$('#boton-eur').click(() => {
+   $('ul').remove();
+   fetch(urlEuro)
 .then(respuesta => respuesta.json())
 .then(respuestajson =>
    {  
    
       Object.keys(respuestajson.rates).forEach(monedas=>{
-       $('#valores').append(`<ul>${monedas} : ${respuestajson.rates[monedas]}</ul>`)
-     // console.log(monedas + " " + respuestajson.rates[monedas]) ;
-   })
+       $('#valores').append(`<ul>${monedas} : ${respuestajson.rates[monedas]}</ul>`)})
       
-      
+     
       $('#moneda').text(`Moneda: ${respuestajson.base}`)
       $('#dia').text(`Dia: ${respuestajson.date}`)
-     // $('#valores').append(`<ul> ${} </ul>`)
+     }) 
+
+.catch(error => console.error("fallo al pedir la informacion",error));
+});
+
+
+$('#boton-usd').click(()=>{
+   
+const urlDolar = "https://api.exchangeratesapi.io/latest?base=USD";
+$('ul').remove();   
+fetch(urlDolar)
+   .then(respuesta => respuesta.json())
+   .then(respuestajson => {
+      Object.keys(respuestajson.rates).forEach(monedas=>{
+         $('#valores').append(`<ul>${monedas} : ${respuestajson.rates[monedas]}</ul>`)})
+
+   $('#moneda').text(`Moneda: ${respuestajson.base}`)
+   $('#dia').text(`Dia: ${respuestajson.date}`)
 })
 .catch(error => console.error("fallo al pedir la informacion",error));
-
+})
 
 
